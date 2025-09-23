a3c34419c4c61116d2f62d30fe6e3bce01956f98
Viet Nguyen <>
2025-09-23 20:15:35 +0700

    add nodemon

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,11 @@ const app = express();
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
 });
+
+app.get("/api/tasks", (req, res) => {
+  const tasks = [
+    { id: 1, title: "Task 1", completed: false },
+    { id: 2, title: "Task 2", completed: true },
+  ];
+  res.status(200).send(tasks);
+});
