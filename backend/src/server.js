3e7df7a14db341acf1b36d6a9d951e6c039d225a
Viet Nguyen <>
2026-01-20 21:51:10 +0700

    Ready to deloy

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,15 +6,28 @@ import { connectDB } from "./config/db.js";
 // Configure dotenv to load from the correct path
 dotenv.config({ path: "./src/.env" });
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-app.use(cors({origin: "http://localhost:5173"}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
+
 app.use(express.json());
 app.use("/api/tasks", tasksRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
